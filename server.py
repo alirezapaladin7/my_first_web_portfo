7dbab8f93f55e9d2b7244b11fe0d1e623b21e3a7
asus labtop <alireza.abdolalipour@gmail.com>
2020-04-12 11:27:23 +0430

    contact with time

--- server.py
+++ server.py
@@ -3,13 +3,15 @@
 
 """
 
-from flask import Flask, render_template, request,redirect
-import csv,time
+from flask import Flask, render_template, request, redirect
+import csv
+import datetime
 
 
 def contact_logger(data):
     with open('contact_list.csv', mode='a', newline='\n') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow(data)
 
 
@@ -34,7 +36,8 @@ def submiter():
     if request.method == "POST":
         data = request.form
         contact_logger([data['name'],
-              data['email'],
-              data['massage']])
+                        data['email'],
+                        data['massage'],
+                        datetime.datetime.now()])
 
     return redirect('./submit_contact.html')
